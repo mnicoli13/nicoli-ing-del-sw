2ddf272f375cb7a33d40b2cea0665d6ad35ac2f5
mnicoli13 <m.nicoli13@studenti.unibg.it>
2024-11-30 16:27:33 +0100

    create backend

---
+++ backend/src/main/java/IngDelSw/nicoli/NicoliApplication.java
@@ -0,0 +1,13 @@
+package IngDelSw.nicoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicoliApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/IngDelSw/nicoli/NicoliApplicationTests.java
@@ -0,0 +1,13 @@
+package IngDelSw.nicoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
